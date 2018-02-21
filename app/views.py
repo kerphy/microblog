813fdbe27fa752dd60640438fb7c6bc3133ed552
root <guohuizou2000@sina.com>
2018-02-22 00:13:20 +0800

    enable templates

--- app/views.py
+++ app/views.py
@@ -1,6 +1,18 @@
 from app import app
-
+from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello,World!"
+	user={'nickname':'Miguel'} #only fake user
+	posts=[
+	{
+		'author':{'nickname':'John'},
+		'body':'beautiful day in Portland!'
+	},
+	{
+		'author':{'nickname':'Susan'},
+		'body':'the Avengers movie was so cool!'
+	}
+	]
+	return render_template('index.html',title='Home',user=user,posts=posts)
+
