bfa8e34d6f9b3871a2711b62a2d3ed056eac41e5
root <guohuizou2000@sina.com>
2018-02-24 01:08:05 +0800

    enable form for login

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
+from flask import render_template,flash,redirect
 from app import app
-from flask import render_template
+from .forms import LoginForm
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -16,3 +18,13 @@ def index():
 	]
 	return render_template('index.html',title='Home',user=user,posts=posts)
 
+@app.route('/login',methods=['GET','POST'])
+def login():
+    form=LoginForm()
+   # print '''rememberme data %d,lable=%s\n''' %(form.remember_me.data,form.remember_me.label)
+    if form.validate_on_submit():
+        flash('Login requested for user {},remember_me={}'.format(form.username.data,form.remember_me.data))
+        print  'redirect to index'
+        return redirect('/index')
+    print 'render_template login.html'
+    return render_template('login.html',title='Sign In',form=form)
